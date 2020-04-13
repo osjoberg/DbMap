b19945abc21e18174271893d7768fee8293e4230
Oskar Sjöberg <oskar@oskarsjoberg.se>
2020-04-13 20:23:53 +0200

    Benchmark suite tests is now easier to determine where it is broken.

--- src/DbMap.Test/BenchmarkSuiteTest.cs
+++ src/DbMap.Test/BenchmarkSuiteTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 using BenchmarkDotNet.Attributes;
 
@@ -16,56 +17,62 @@ namespace DbMap.Test
     {
         [TestMethod]
         [DynamicData(nameof(GetTinyBenchmarkResults), DynamicDataSourceType.Method)]
-        public void TinyBenchmarkReturnsCorrectResult(string name, string item)
+        public void TinyBenchmarkReturnsCorrectResult(string name, MethodInfo method, object instance)
         {
             var expected = Tiny.Create(0).String;
+            var actual = (string)method.Invoke(instance, null);
 
-            Assert.AreEqual(expected, item, name);
+            Assert.AreEqual(expected, actual, name);
         }
 
         [TestMethod]
         [DynamicData(nameof(GetExtraSmallBenchmarkResults), DynamicDataSourceType.Method)]
-        public void ExtraSmallBenchmarkReturnsCorrectResult(string name, ExtraSmall item)
+        public void ExtraSmallBenchmarkReturnsCorrectResult(string name, MethodInfo method, object instance)
         {
             var expected = ExtraSmall.Create(0);
+            var actual = (ExtraSmall)method.Invoke(instance, null);
 
-            Assert.AreEqual(expected, item, name);
+            Assert.AreEqual(expected, actual, name);
         }
 
         [TestMethod]
         [DynamicData(nameof(GetSmallBenchmarkResults), DynamicDataSourceType.Method)]
-        public void SmallBenchmarkReturnsCorrectResult(string name, List<Small> list)
+        public void SmallBenchmarkReturnsCorrectResult(string name, MethodInfo method, object instance)
         {
             var expected = Enumerable.Range(0, 10).Select(Small.Create).ToList();
+            var actual = (List<Small>)method.Invoke(instance, null);
 
-            CollectionAssert.AreEqual(expected, list, name);
+            CollectionAssert.AreEqual(expected, actual, name);
         }
 
         [TestMethod]
         [DynamicData(nameof(GetMediumBenchmarkResults), DynamicDataSourceType.Method)]
-        public void MediumBenchmarkReturnsCorrectResult(string name, List<Medium> list)
+        public void MediumBenchmarkReturnsCorrectResult(string name, MethodInfo method, object instance)
         {
             var expected = Enumerable.Range(0, 100).Select(Medium.Create).ToList();
+            var actual = (List<Medium>)method.Invoke(instance, null);
 
-            CollectionAssert.AreEqual(expected, list, name);
+            CollectionAssert.AreEqual(expected, actual, name);
         }
 
         [TestMethod]
         [DynamicData(nameof(GetLargeBenchmarkResults), DynamicDataSourceType.Method)]
-        public void LargeBenchmarkReturnsCorrectResult(string name, List<Large> list)
+        public void LargeBenchmarkReturnsCorrectResult(string name, MethodInfo method, object instance)
         {
             var expected = Enumerable.Range(0, 1000).Select(Large.Create).ToList();
+            var actual = (List<Large>)method.Invoke(instance, null);
 
-            CollectionAssert.AreEqual(expected, list, name);
+            CollectionAssert.AreEqual(expected, actual, name);
         }
 
         [TestMethod]
         [DynamicData(nameof(GetExtraLargeBenchmarkResults), DynamicDataSourceType.Method)]
-        public void ExtraLargeBenchmarkReturnsCorrectResult(string name, List<ExtraLarge> list)
+        public void ExtraLargeBenchmarkReturnsCorrectResult(string name, MethodInfo method, object instance)
         {
             var expected = Enumerable.Range(0, 10000).Select(ExtraLarge.Create).ToList();
+            var actual = (List<ExtraLarge>)method.Invoke(instance, null);
 
-            CollectionAssert.AreEqual(expected, list, name);
+            CollectionAssert.AreEqual(expected, actual, name);
         }
 
         private static IEnumerable<object[]> GetTinyBenchmarkResults() => GetBenchmarkResults(typeof(TinyBenchmark));
@@ -93,7 +100,7 @@ namespace DbMap.Test
             return benchmarkType
                 .GetMethods()
                 .Where(method => method.GetCustomAttributes(typeof(BenchmarkAttribute), false).Any())
-                .Select(method => new[] { method.Name, method.Invoke(benchmarkInstance, null) });
+                .Select(method => new[] { method.Name, method, benchmarkInstance });
         }
     }
 }
